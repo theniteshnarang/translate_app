33be66963dc8085bb48fa23f3be06a115f055051
theniteshnarang <narangnitesh@ymail.com>
2020-12-25 10:06:09 +0530

    style: added styling

--- app.js
+++ app.js
@@ -3,18 +3,18 @@ var textarea_ref = document.querySelector('textarea')
 var output_ref = document.querySelector('#outputHere')
 var mock_url = 'https://api.funtranslations.com/translate/mandalorian.json'
 
-var query='text'
+var query = 'text'
 
 // textarea_ref.addEventListener('change', handleText)
 button_ref.addEventListener('click', clickHandler)
 
-function urlGenerator(mock_url,query,text){
-    return mock_url+'?'+query+'='+text
+function urlGenerator(mock_url, query, text) {
+    return mock_url + '?' + query + '=' + text
 }
 
 function clickHandler() {
-    var text= textarea_ref.value
-    const url= urlGenerator(mock_url,query,text)
+    var text = textarea_ref.value
+    const url = urlGenerator(mock_url, query, text)
     console.log(url)
     doFetch(url)
 }
